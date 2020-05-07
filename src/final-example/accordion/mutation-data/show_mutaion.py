a00434cf67221af845f3dff1d78a8b9433eaeb18
fireHedgehog <rj.chen91@gmail.com>
2020-05-07 21:23:16 +1200

    gacnn example commit

--- src/final-example/accordion/mutation-data/show_mutaion.py
+++ src/final-example/accordion/mutation-data/show_mutaion.py
@@ -14,31 +14,31 @@ def get_data_by_file_name(name):
     return pd.DataFrame(data, columns=cols)
 
 
-df = get_data_by_file_name('01-07-50-100.txt')
-df1 = get_data_by_file_name('01-07-100-300.txt')
-df1 = df1.head(110)
-df2 = get_data_by_file_name('01-07-150.txt')
+df = get_data_by_file_name('../population-data/01-07-100-300.txt')
+df = df.head(110)
+df1 = get_data_by_file_name('mutation-02.txt')
+df2 = get_data_by_file_name('03-mutaion.txt')
 
 print(df.describe())
 print(df1.describe())
 print(df2.describe())
 
 fig, axs = plt.subplots(1, 2, figsize=(20, 5))
-fig.suptitle('Population Size Comparision for Accordion Encoding ( CIFAR-10 )', y=1, fontsize=16)
+fig.suptitle('Mutation Rate Comparision for Accordion Encoding ( CIFAR-10 )', y=1, fontsize=16)
 
-axs[0].plot(df[['best_acc']], marker='_', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=3)
-axs[0].plot(df1[['accuracy']], marker='.', markerfacecolor='red', markersize=12, color='orange', linewidth=3)
+axs[0].plot(df[['accuracy']], marker='_', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=3)
+axs[0].plot(df1[['best_acc']], marker='.', markerfacecolor='red', markersize=12, color='orange', linewidth=3)
 axs[0].plot(df2[['best_acc']], marker='+', markerfacecolor='black', markersize=12, color='green', linewidth=3)
 
-axs[0].legend(['Pop_size=50', 'Pop_size=100', 'Pop_size=150'], loc='upper left')
+axs[0].legend(['Mutation Rate=0.1', 'Mutation Rate=0.2', 'Mutation Rate=0.3'], loc='upper left')
 axs[0].set_title('Accuracy')
 axs[0].set_xlabel('Generations')
 axs[0].set_ylabel('Accuracy')
 
-axs[1].plot(df[['best_loss']], marker='_', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=3)
-axs[1].plot(df1[['loss']], marker='.', markerfacecolor='red', markersize=12, color='orange', linewidth=3)
+axs[1].plot(df[['loss']], marker='_', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=3)
+axs[1].plot(df1[['best_loss']], marker='.', markerfacecolor='red', markersize=12, color='orange', linewidth=3)
 axs[1].plot(df2[['best_loss']], marker='+', markerfacecolor='black', markersize=12, color='green', linewidth=3)
-axs[1].legend(['Pop_size=50', 'Pop_size=100', 'Pop_size=150'], loc='upper left')
+axs[1].legend(['Mutation Rate=0.1', 'Mutation Rate=0.2', 'Mutation Rate=0.3'], loc='upper left')
 axs[1].set_title('Loss')
 axs[1].set_xlabel('Generations')
 axs[1].set_ylabel('Loss')
