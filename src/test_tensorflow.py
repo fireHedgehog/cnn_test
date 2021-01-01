f014979b7a3482a5733bfc08a4ce43c6a785f032
fireHedgehog <rj.chen91@gmail.com>
2021-01-02 11:03:11 +1300

    gacnn example commit

--- src/test_tensorflow.py
+++ src/test_tensorflow.py
@@ -1,8 +1,20 @@
-import tensorflow.compat.v1 as tf
+# import tensorflow.compat.v1 as tf
+import torch
 
-print (tf.__version__)
-tf.config.list_physical_devices('GPU')
-# tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
+""""
+def test_tf_cuda():
+    # tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
 
-# gpu_options = tf.GPUOptions(allow_growth=True)
-# session_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False, gpu_options=gpu_options)
+    # gpu_options = tf.GPUOptions(allow_growth=True)
+    # session_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False, gpu_options=gpu_options)
+
+    print(tf.__version__)
+    tf.config.list_physical_devices('GPU')
+"""
+
+
+def test_torch_cuda():
+    print(torch.cuda.is_available())
+
+
+test_torch_cuda()
