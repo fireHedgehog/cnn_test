1ec21f0b5207689b2fe7f69dae6ffa3d912d5194
fireHedgehog <rj.chen91@gmail.com>
2021-01-03 12:22:49 +1300

    gacnn example commit

--- src/pytorch/nn_bp_to_ga.py
+++ src/pytorch/nn_bp_to_ga.py
@@ -6,6 +6,9 @@ device = torch.device("cpu")
 if torch.cuda.is_available():
     device = torch.device("cuda")  # a CUDA device object
 
+print(torch.cuda.is_available())
+print(device)
+
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
 N, D_in, H, D_out = 64, 1000, 100, 10
@@ -44,4 +47,4 @@ def bp_nn(w1, w2):
         w2 -= learning_rate * grad_w2
 
 
-bp_nn(w1=w_1, w2=w_2)
+# bp_nn(w1=w_1, w2=w_2)
